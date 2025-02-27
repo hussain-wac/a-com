9709263bb16b0f657dc5b93269469ccd5c433002
hussain-wac <hussain.n@webandcrafts.in>
2025-02-27 16:52:34 +0530

    search removed

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -10,19 +10,6 @@ function Topbar() {
         <div className="text-white font-extrabold text-2xl">
           <Link href="/">E-Shop</Link>
         </div>
-
-        {/* Search Bar */}
-        <div className="flex flex-1 mx-4">
-            <input 
-              type="text"
-            placeholder="Search products..."
-            className="w-full px-4 py-2 rounded-l-md focus:outline-none"
-          />
-          <button className="bg-yellow-500 hover:bg-yellow-600 text-white px-4 rounded-r-md flex items-center justify-center">
-            <Search size={16} />
-          </button>
-        </div>
-
         {/* Icons Section */}
         <div className="flex items-center space-x-6 text-white">
           <Link href="/about" className="hover:underline">
