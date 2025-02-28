ce46bcadce57bcf4724db0a3be7ac76dd95fe162
hussain-wac <hussain.n@webandcrafts.in>
2025-02-28 15:01:35 +0530

    api

--- components/DefaultTopbar.jsx
+++ components/DefaultTopbar.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import { AppBar, Toolbar, Typography, IconButton, Badge, Box, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, IconButton, Badge, Button } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 function DefaultTopbar() {
   return (
     <AppBar position="static" sx={{ backgroundColor: "#1E88E5" }}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
+      <Toolbar sx={{ justifyContent: "space-between", px: 2 }}>
         {/* Logo Section */}
-        <Typography variant="h6" component={Link} href="/" sx={{ textDecoration: "none", color: "white", fontWeight: "bold" }}>
-        A-com
+        <Typography
+          variant="h6"
+          component={Link}
+          href="/"
+          sx={{ textDecoration: "none", color: "white", fontWeight: "bold" }}
+        >
+          A-com
         </Typography>
 
-        {/* Icons Section */}
-        <Box display="flex" alignItems="center" gap={3}>
-          <Button component={Link} href="/about" sx={{ color: "white" }}>
+        {/* Icons and Buttons Section */}
+        <div>
+          <Button component={Link} href="/about" sx={{ color: "white", mr: 2 }}>
             About
           </Button>
           <IconButton color="inherit">
@@ -21,10 +27,10 @@ function DefaultTopbar() {
               <ShoppingCartIcon />
             </Badge>
           </IconButton>
-          <Button component={Link} href="/signup" sx={{ color: "white" }}>
+          <Button component={Link} href="/signup" sx={{ color: "white", ml: 2 }}>
             Signup
           </Button>
-        </Box>
+        </div>
       </Toolbar>
     </AppBar>
   );
